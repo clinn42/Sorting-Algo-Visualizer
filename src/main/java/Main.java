caaa21a72fde2d888b64300a4a37791aab6ebac1
Collin Pereira <67064169+clinn42@users.noreply.github.com>
2020-08-03 14:07:47 +0530

    finally added maven suppot: org_openjfx_javafx_archetype_fxml_0_0_5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         stage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("template.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/template.fxml"));
         primaryStage.setTitle("Visualizer");
         //primaryStage.setFullScreen(true);
         //primaryStage.setMaximized(true);
